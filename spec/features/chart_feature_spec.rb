92a4abbc76deb2999a7f2588214b175ed25e08bf
Mario Gintili <mariojgintili@gmail.com>
2014-04-25 14:38:45 +0100

    Fixed spec for file input form

--- spec/features/chart_feature_spec.rb
+++ spec/features/chart_feature_spec.rb
@@ -11,7 +11,7 @@ describe "Building a chart" do
 		it "shows the form to create one" do
 			fill_in('Name', with: "Football chart")
 			page.choose('chart_chart_type_pie_chart')
-			attach_file('Upload csv', Rails.root.join("spec/extras/test.csv"))
+			attach_file('chart_csv', Rails.root.join("spec/extras/test.csv"))
 			click_button "Generate"
 			expect(page).to have_css ".chart_container"
 		end
