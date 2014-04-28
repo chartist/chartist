faf880409212c07cad54ff4574fd29b98dfce721
Mikhail Dubov <mdubov@gmail.com>
2014-04-28 15:04:06 +0100

    Added a test for CSV processing

---
+++ spec/csvprocessor_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "CsvProcessor" do
+
+  let(:processor) { CSVProcessor.new(Rails.root.join('spec/extras/test.csv')) }
+
+  it "formats a csv file into an array of hashes" do
+    expect(processor.process.first).to eq({travel: "US", value: 0.75})
+  end
+end
