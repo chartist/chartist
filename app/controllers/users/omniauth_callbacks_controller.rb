6e7e5985b1602aa76fda9cca219d9c3768daeb3c
Mario Gintili <mariojgintili@gmail.com>
2014-04-29 16:27:30 +0100

    added controller for google auth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -13,5 +13,18 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
+  def google_oauth2
+    @user = User.find_for_oauth(request.env["omniauth.auth"])
+    provider = @user.provider
+
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication
+      set_flash_message(:notice, :success, :kind => provider.capitalize) if is_navigational_format?
+    else
+      session["devise.#{provider}_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
   alias_method :linkedin, :facebook
 end
