bc74fa03d6b3d010249f0387c9684e7ba9fa62d1
Mario Gintili <mariojgintili@gmail.com>
2014-04-30 15:06:26 +0100

    refactored charts partial

--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -1,2 +1,9 @@
 module ChartsHelper
+
+	def graphic_of(chart)
+		options = { id: "#{chart.chart_type}_#{chart.id}" }
+		options.merge! library: { :hAxis => {:format => 'd-MMM-yy'}} if chart.line_chart?
+
+		send(chart.chart_type, chart_path(chart, format: :json), options).html_safe
+	end
 end
