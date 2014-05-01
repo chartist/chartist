5098f9093b1b6c8a2edca9ca6bf8c840f9e890b0
Mario Gintili <mariojgintili@gmail.com>
2014-05-01 16:07:43 +0100

    added route for widget

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root to: "landing#index"
-  # root to: "home#index"
+  
+  root to: "charts#index"
+
   get 'secret_homepage' => 'home#index'
   resources :dashboards, only: [:show]
   resources :charts do
