70eb6566424cba3af9638df3af155f7726fcc493
itsmurasaki <itsmurasaki@gmail.com>
2014-05-02 12:23:07 +0100

    added buttons in the home

---
+++ app/assets/javascripts/Chartist_files/landing-page.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function(){
+
+
+  /* ---- Countdown timer ---- */
+
+  if($('#counter').length) { 
+    $('#counter').countdown({
+      timestamp : new Date("2014-05-09T16:00:00Z")
+    });
+  }
+
+  /* ---- Animations ---- */
+
+  $('#links a').hover(
+    function(){ $(this).animate({ left: 3 }, 'fast'); },
+    function(){ $(this).animate({ left: 0 }, 'fast'); }
+  );
+
+  $('footer a').hover(
+    function(){ $(this).animate({ top: 3 }, 'fast'); },
+    function(){ $(this).animate({ top: 0 }, 'fast'); }
+  );
+
+});
