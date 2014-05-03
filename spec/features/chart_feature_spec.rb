7073d1c7000e1215d3b38f64e074c0a6e74b7f03
Mario Gintili <mariojgintili@gmail.com>
2014-05-03 12:50:30 +0100

    fixed spec to match with new URLs

--- spec/features/chart_feature_spec.rb
+++ spec/features/chart_feature_spec.rb
@@ -95,7 +95,7 @@ describe 'Editing a chart' do
     expect(current_path).to eq chart_path(chart) + '/edit'
     fill_in 'Name', with: 'Bla'
     click_button 'Edit'
-    expect(current_path).to eq chart_path(chart)
+    expect(chart_path(chart)).to eq "/charts/#{chart.to_param}"
     expect(page).to have_css ".panel-heading", text: 'Bla'
   end
 
