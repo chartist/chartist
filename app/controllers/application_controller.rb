46835096d10e71fc8e3c762a6b8f6de1f845fca0
Mario Gintili <mariojgintili@gmail.com>
2014-05-06 15:50:57 +0100

    overwrote devise method to redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,5 +14,9 @@ class ApplicationController < ActionController::Base
     # devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :bio) }
   end
 
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || charts_path
+  end
+
 end
 
