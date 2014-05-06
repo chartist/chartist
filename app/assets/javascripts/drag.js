df18db0ed569a0055cab056d0c825098badf2265
Mikhail Dubov <mdubov@gmail.com>
2014-05-06 14:24:43 +0100

    Added drag and drop to dashboards using Gridster.js

--- app/assets/javascripts/drag.js
+++ app/assets/javascripts/drag.js
@@ -1,7 +1,6 @@
 $(function(){ //DOM Ready
     $(".gridster ul").gridster({
         widget_margins: [10, 10],
-        widget_base_dimensions: [140, 140]
+        widget_base_dimensions: [350, 400]
     });
- 
 });
