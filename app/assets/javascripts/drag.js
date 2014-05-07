31546e5244a1a421268cbc728848b7e216c39989
Mikhail Dubov <mdubov@gmail.com>
2014-05-07 17:20:25 +0100

    Changed styles on dashboard page

--- app/assets/javascripts/drag.js
+++ app/assets/javascripts/drag.js
@@ -1,6 +1,7 @@
 $(function(){ //DOM Ready
     $(".gridster ul").gridster({
         widget_margins: [10, 10],
-        widget_base_dimensions: [350, 400]
+        widget_base_dimensions: [350, 400],
+        
     });
 });
