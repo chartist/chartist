ad4ee12b2ee79ea1e7f1abfd5b1dd240ca2cdb1b
Mikhail Dubov <mdubov@gmail.com>
2014-05-07 12:59:30 +0100

    Added pin functionality for charts

--- spec/features/pin_feature_spec.rb
+++ spec/features/pin_feature_spec.rb
@@ -1,17 +1,17 @@
 require "spec_helper"
 
 describe 'Pinning charts to dashboard' do
-	context 'Adds the chart to default dashboard for the user' do
-		
-		let(:user) { create(:user) }
-		let!(:chart) { create(:chart_with_dashboards, user: user) }
-		let(:user2) {create(:hacker)}
+  context 'Adds the chart to default dashboard for the user' do
 
-		it 'when the pin button is pressed' do
-			login_as user2
-			visit chart_path(chart)
-			click_link 'pin'
-			expect(chart.dashboards.map(&:title)).to include 'bar'
-		end
-	end
+    let(:user) { create(:user) }
+    let!(:chart) { create(:chart_with_dashboards, user: user) }
+    let(:user2) {create(:hacker)}
+
+    it 'when the pin button is pressed' do
+      login_as user2
+      visit chart_path(chart)
+      click_link 'pin'
+      expect(chart.dashboards.map(&:title)).to include 'bar'
+    end
+  end
 end
