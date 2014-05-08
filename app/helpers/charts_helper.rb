15771b87a7872f37cdcf699db6760099c5e0248a
Mario Gintili <mariojgintili@gmail.com>
2014-05-08 10:58:03 +0100

    fixed colorscheme so data will contrast

--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -9,7 +9,7 @@ module ChartsHelper
   def color_select(colorscheme)
     colors = {
       'spring' => ['#4EB8E6', '#EA729A', '#FDD017', '#9DC209', '#B1B1B1'],
-      'summer' => ['#E20E0B', '#FFE833', '#BED334', '#4C9BDB', '#B5CEE2'],
+      'summer' => ["#c0392b", "#3498db", "#E20E0B", "#BED334", "#B5CEE2"] ,
       'autumn' => ['#305E5C', '#6D833D', '#FCC362', '#F2872B', '#D36613'],
       'winter' => ['#606A82', '#93110A', '#C91000', '#B7915F', '#DDCABC']
     }
